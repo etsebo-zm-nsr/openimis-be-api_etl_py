ca224b7059bf062e4f092d96ad18df3f9c906151
Wei Lu <luwei.here@gmail.com>
2025-01-20 03:37:35 -0500

    Fix: update ETL service base to facilitate batch processing (#5)
    * convert ETL service integ test to a unit test with mock API responses
    
    * fix: update ETL service base to facilitate batch processing
    
    * fix: unit tests & enforce transform input param to be iterable

--- api_etl/adapters/exampleIndividialAdapter.py
+++ api_etl/adapters/exampleIndividialAdapter.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Iterable
 
 from api_etl.adapters.base import DataAdapter
 from api_etl.apps import ApiEtlConfig
@@ -9,23 +9,22 @@ class ExampleIndividualAdapter(DataAdapter):
     Adapter allowing data transformation form ExampleIndividualSource to IndividualService input
     """
 
-    def transform(self, data: Any) -> Any:
+    def transform(self, data: Iterable[Any]) -> Iterable[Any]:
         """
         Transform paginated result of the ExampleIndividualSource
         Input data is assumed to be Iterable of pages from the example API
         """
         result = []
 
-        for page in data:
-            if not "rows" in page:
-                raise self.Error("Invalid input, rows field required")
+        if data is None:
+            raise self.Error(f"Invalid input, expect input not to be None")
 
-            for row in page["rows"]:
-                row.pop("id", None)
-                result_row = {"first_name": row.pop(ApiEtlConfig.adapter_first_name_field) or "empty",
-                              "last_name": row.pop(ApiEtlConfig.adapter_last_name_field) or "empty",
-                              "dob": row.pop(ApiEtlConfig.adapter_dob_field) or "1970-01-01",
-                              "json_ext": row}
-                result.append(result_row)
+        for row in data:
+            row.pop("id", None)
+            result_row = {"first_name": row.pop(ApiEtlConfig.adapter_first_name_field) or "empty",
+                          "last_name": row.pop(ApiEtlConfig.adapter_last_name_field) or "empty",
+                          "dob": row.pop(ApiEtlConfig.adapter_dob_field) or "1970-01-01",
+                          "json_ext": row}
+            result.append(result_row)
 
         return result
